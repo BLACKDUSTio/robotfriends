792f36a2614c872f7ea86b0cb3b1be7aa15f93b4
Carl Lacharite <clacharite@blackdust.io>
2024-01-31 13:00:23 -0500

    Avec correction 31 janvier

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -2,18 +2,15 @@ import React from 'react';
 import Card from './Card';
 
 const CardList = ({ robots }) => {
-  if (true) {
-    throw new Error('NOOOOOOO!');
-  }
   return (
     <div>
       {
-        robots.map((user,i) => {
+        robots.map((user, i) => {
           return (
-            <Card 
+            <Card
               key={i}
-              id={robots[i].id} 
-              name={robots[i].name} 
+              id={robots[i].id}
+              name={robots[i].name}
               email={robots[i].email}
               />
           );
